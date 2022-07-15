2a0f4543a13b772535e9b9d84f1a92b60af15e59
Thiago Pereira <thiago.pereira@onearth.com.br>
2022-07-15 20:47:54 +0000

    Incluindo campo para o numero do cartao

--- client/src/components/PaymentForm.js
+++ client/src/components/PaymentForm.js
@@ -1,3 +1,13 @@
+import { CardElement } from "@stripe/react-stripe-js";
+
 export default function PaymentForm() {
-  return <div>Payment form</div>;
+  return (
+    <div>
+      <h2>Payment form</h2>
+
+      <div>
+        Card: <CardElement />
+      </div>
+    </div>
+  );
 }
