c27a908babc28e0988efb33761374a37aa88de9f
Thiago Pereira <thiago.pereira@onearth.com.br>
2022-07-20 18:12:12 +0000

    Criando o terceiro componente de pagamento

---
+++ client/src/views/forms/CardPayment2.tsx
@@ -0,0 +1,3 @@
+export default function CardPayment2() {
+  return <div>Card2</div>
+}
--- client/src/views/forms/PaymentForm.tsx
+++ client/src/views/forms/PaymentForm.tsx
@@ -2,15 +2,35 @@ import { useState } from 'react'
 import { Box, FormControl, InputLabel, Select, MenuItem } from '@mui/material'
 
 import CardPayment from './CardPayment'
+import CardPayment2 from './CardPayment2'
 import BoletoPayment from './BoletoPayment'
 
 enum PaymentType {
   CARD = 1,
-  BOLETO = 2
+  BOLETO = 2,
+  CARD2 = 3
 }
 
 export default function PaymentForm() {
   const [paymentType, setPaymentType] = useState<PaymentType>(PaymentType.CARD)
+  let template: any
+
+  switch (paymentType) {
+    case PaymentType.CARD:
+      template = <CardPayment />
+      break
+
+    case PaymentType.CARD2:
+      template = <CardPayment2 />
+      break
+
+    case PaymentType.BOLETO:
+      template = <BoletoPayment />
+      break
+
+    default:
+      break
+  }
 
   return (
     <Box>
@@ -25,12 +45,13 @@ export default function PaymentForm() {
             onChange={e => setPaymentType(Number(e.target.value))}
           >
             <MenuItem value={PaymentType.CARD}>CARTAO</MenuItem>
+            <MenuItem value={PaymentType.CARD2}>CARTAO2</MenuItem>
             <MenuItem value={PaymentType.BOLETO}>BOLETO</MenuItem>
           </Select>
         </FormControl>
       </Box>
 
-      {paymentType === PaymentType.CARD ? <CardPayment /> : <BoletoPayment />}
+      {template}
     </Box>
   )
 }
